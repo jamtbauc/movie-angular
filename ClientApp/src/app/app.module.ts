e67c49ca689ce7333a89c949de1e263c101f86e2
James Baucum <jamestbauc@gmail.com>
2022-07-27 22:15:14 -0500

    Add fetch-movie component to router module and navbar

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { FetchMoviesComponent } from './fetch-movies/fetch-movies.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
     NavMenuComponent,
     HomeComponent,
     CounterComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    FetchMoviesComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,6 +28,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: '', component: HomeComponent, pathMatch: 'full' },
       { path: 'counter', component: CounterComponent },
       { path: 'fetch-data', component: FetchDataComponent },
+      { path: 'fetch-data', component: FetchMoviesComponent }
     ])
   ],
   providers: [],
