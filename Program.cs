604a7a1aca000802da889feb10b6a2f95a54b025
James Baucum <jamestbauc@gmail.com>
2022-07-27 21:39:00 -0500

    Scaffold controller and dbcontext

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MovieContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MovieContext") ?? throw new InvalidOperationException("Connection string 'MovieContext' not found.")));
 
 // Add services to the container.
 
