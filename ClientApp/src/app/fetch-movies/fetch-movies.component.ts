e1580c0edde1ad7af13f7061eb9f9c3f83bc0ed4
James Baucum <jamestbauc@gmail.com>
2022-07-27 22:11:51 -0500

    Add fetch-movies component

---
+++ ClientApp/src/app/fetch-movies/fetch-movies.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fetch-movies',
+  templateUrl: './fetch-movies.component.html',
+  styleUrls: ['./fetch-movies.component.css']
+})
+export class FetchMoviesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
